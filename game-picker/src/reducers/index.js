a219d36ffc61cd1e1c466330e557cdf815cd7370
Mckay <mckaywrigley@gmail.com>
2019-03-10 21:34:16 -0700

    adds playoffReducer

--- game-picker/src/reducers/index.js
+++ game-picker/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import gameReducer from './gameReducer';
+import playoffReducer from './playoffReducer';
 
 export default combineReducers({
-    gameReducer
+    gameReducer,
+    playoffReducer
 });
---
+++ game-picker/src/reducers/playoffReducer.js
@@ -0,0 +1,37 @@
+import {
+    SELECT_GAME
+} from '../actions';
+
+const initialState = {
+    teams: [
+        { location: 'Golden State', name: 'Warriors', conference: 'West', seed: 1 },
+        { location: 'Denver', name: 'Nuggets', conference: 'West', seed: 2 },
+        { location: 'Houston', name: 'Rockets', conference: 'West', seed: 3 },
+        { location: 'Oklahoma City', name: 'Thunder', conference: 'West', seed: 4 },
+        { location: 'Portland', name: 'Trail Blazers', conference: 'West', seed: 5 },
+        { location: 'Utah', name: 'Jazz', conference: 'West', seed: 6 },
+        { location: 'Los Angeles', name: 'Clippers', conference: 'West', seed: 7 },
+        { location: 'San Antonio', name: 'Spurs', conference: 'West', seed: 8 },
+        { location: 'Milwaukee', name: 'Bucks', conference: 'East', seed: 1 },
+        { location: 'Toronto', name: 'Raptors', conference: 'East', seed: 2 },
+        { location: 'Philadelphia', name: '76ers', conference: 'East', seed: 3 },
+        { location: 'Indiana', name: 'Pacers', conference: 'East', seed: 4 },
+        { location: 'Boston', name: 'Celtics', conference: 'East', seed: 5 },
+        { location: 'Detroit', name: 'Pistons', conference: 'East', seed: 6 },
+        { location: 'Brooklyn', name: 'Nets', conference: 'East', seed: 7 },
+        { location: 'Miami', name: 'Heat', conference: 'East', seed: 8 }
+    ]
+};
+
+function playoffReducer(state = initialState, action) {
+    switch(action.type) {
+        case SELECT_GAME: 
+            return {
+                ...state,
+            };
+        default:
+            return state;
+    }
+};
+
+export default playoffReducer;
