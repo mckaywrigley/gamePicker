148e29a503c8b665c5e4e67d7a2fce4b2972b052
Mckay <mckaywrigley@gmail.com>
2019-03-10 19:02:09 -0700

    adds combineReducers

--- game-picker/src/index.js
+++ game-picker/src/index.js
@@ -7,10 +7,10 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-import rootReducer from './reducers';
+import combineReducers from './reducers';
 
 const store = createStore(
-    rootReducer, 
+    combineReducers, 
     applyMiddleware(thunk, logger)
 );
 
