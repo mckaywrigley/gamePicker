9c6d93c93371aa233b7344d0dbe53829a0533972
Mckay <mckaywrigley@gmail.com>
2019-03-16 14:39:03 -0700

    compartmentalizes teams as buttons

--- nbaPlayoff19PickerApp/src/views/PlayoffBracketView.js
+++ nbaPlayoff19PickerApp/src/views/PlayoffBracketView.js
@@ -22,45 +22,45 @@ class PlayoffBracketView extends Component {
                 <div className="west">
                     <div className="west-first-round">
                         <h3>West 1st Round</h3>
-                        <p>{this.props.teams[0].name} vs. {this.props.teams[1].name}</p>
-                        <p>{this.props.teams[2].name} vs. {this.props.teams[3].name}</p>
-                        <p>{this.props.teams[4].name} vs. {this.props.teams[5].name}</p>
-                        <p>{this.props.teams[6].name} vs. {this.props.teams[7].name}</p>
+                        <p><button>{this.props.teams[0].name}</button><button>{this.props.teams[1].name}</button></p>
+                        <p><button>{this.props.teams[2].name}</button><button>{this.props.teams[3].name}</button></p>
+                        <p><button>{this.props.teams[4].name}</button><button>{this.props.teams[5].name}</button></p>
+                        <p><button>{this.props.teams[6].name}</button><button>{this.props.teams[7].name}</button></p>
                     </div>
                     <div className="west-semis">
                         <h3>West Semis</h3>
-                        <p>{this.props.teams[0].name} vs. {this.props.teams[1].name}</p>
-                        <p>{this.props.teams[2].name} vs. {this.props.teams[3].name}</p>
+                        <p><button>{this.props.teams[0].name}</button><button>{this.props.teams[1].name}</button></p>
+                        <p><button>{this.props.teams[2].name}</button><button>{this.props.teams[3].name}</button></p>
                     </div>
                     <div className="west-finals">
                         <h3>West Finals</h3>
-                        <p>{this.props.teams[0].name} vs. {this.props.teams[1].name}</p>
+                        <p><button>{this.props.teams[0].name}</button><button>{this.props.teams[1].name}</button></p>
                     </div>
                     <div className="west-winner">
                         <h3>West Winner</h3>
-                        <p>{this.props.teams[0].name}</p>
+                        <p><button>{this.props.teams[0].name}</button></p>
                     </div>
                 </div>
                 <div className="east">
                     <div className="east-first-round">
                         <h3>East 1st Round</h3>
-                        <p>{this.props.teams[8].name} vs. {this.props.teams[9].name}</p>
-                        <p>{this.props.teams[10].name} vs. {this.props.teams[11].name}</p>
-                        <p>{this.props.teams[12].name} vs. {this.props.teams[13].name}</p>
-                        <p>{this.props.teams[14].name} vs. {this.props.teams[15].name}</p>
+                        <p><button>{this.props.teams[8].name}</button><button>{this.props.teams[9].name}</button></p>
+                        <p><button>{this.props.teams[10].name}</button><button>{this.props.teams[11].name}</button></p>
+                        <p><button>{this.props.teams[12].name}</button><button>{this.props.teams[13].name}</button></p>
+                        <p><button>{this.props.teams[14].name}</button><button>{this.props.teams[15].name}</button></p>
                     </div>
                     <div className="east-semis">
                         <h3>East Semis</h3>
-                        <p>{this.props.teams[8].name} vs. {this.props.teams[9].name}</p>
-                        <p>{this.props.teams[10].name} vs. {this.props.teams[11].name}</p>
+                        <p><button>{this.props.teams[8].name}</button><button>{this.props.teams[9].name}</button></p>
+                        <p><button>{this.props.teams[10].name}</button><button>{this.props.teams[11].name}</button></p>
                     </div>
                     <div className="east-finals">
                         <h3>East Finals</h3>
-                        <p>{this.props.teams[8].name} vs. {this.props.teams[9].name}</p>
+                        <p><button>{this.props.teams[8].name}</button><button>{this.props.teams[9].name}</button></p>
                     </div>
                     <div className="east-winner">
                         <h3>East Winner</h3>
-                        <p>{this.props.teams[8].name}</p>
+                        <p><button>{this.props.teams[8].name}</button></p>
                     </div>                    
                 </div>
             </div>
