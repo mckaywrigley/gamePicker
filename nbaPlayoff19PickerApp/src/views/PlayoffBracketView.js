65dc058d4e6edc358a1c4b4fe758b2a880b0303a
Mckay <mckaywrigley@gmail.com>
2019-03-16 14:22:29 -0700

    adds in initial bracket style for testing functionality

--- nbaPlayoff19PickerApp/src/views/PlayoffBracketView.js
+++ nbaPlayoff19PickerApp/src/views/PlayoffBracketView.js
@@ -15,19 +15,53 @@ class PlayoffBracketView extends Component {
         return (
             <div className="playoff-bracket-view">
                 <h2>Playoff Bracket</h2>
+                    <div className="champion">
+                        <h3>Champion</h3>
+                        <p>{this.props.teams[0].name}</p>
+                    </div>
                 <div className="west">
-                    <h3>West</h3>
-                    <p>{this.props.teams[0].name} vs. {this.props.teams[1].name}</p>
-                    <p>{this.props.teams[2].name} vs. {this.props.teams[3].name}</p>
-                    <p>{this.props.teams[4].name} vs. {this.props.teams[5].name}</p>
-                    <p>{this.props.teams[6].name} vs. {this.props.teams[7].name}</p>
+                    <div className="west-first-round">
+                        <h3>West 1st Round</h3>
+                        <p>{this.props.teams[0].name} vs. {this.props.teams[1].name}</p>
+                        <p>{this.props.teams[2].name} vs. {this.props.teams[3].name}</p>
+                        <p>{this.props.teams[4].name} vs. {this.props.teams[5].name}</p>
+                        <p>{this.props.teams[6].name} vs. {this.props.teams[7].name}</p>
+                    </div>
+                    <div className="west-semis">
+                        <h3>West Semis</h3>
+                        <p>{this.props.teams[0].name} vs. {this.props.teams[1].name}</p>
+                        <p>{this.props.teams[2].name} vs. {this.props.teams[3].name}</p>
+                    </div>
+                    <div className="west-finals">
+                        <h3>West Finals</h3>
+                        <p>{this.props.teams[0].name} vs. {this.props.teams[1].name}</p>
+                    </div>
+                    <div className="west-winner">
+                        <h3>West Winner</h3>
+                        <p>{this.props.teams[0].name}</p>
+                    </div>
                 </div>
                 <div className="east">
-                <h3>East</h3>
-                    <p>{this.props.teams[8].name} vs. {this.props.teams[9].name}</p>
-                    <p>{this.props.teams[10].name} vs. {this.props.teams[11].name}</p>
-                    <p>{this.props.teams[12].name} vs. {this.props.teams[13].name}</p>
-                    <p>{this.props.teams[14].name} vs. {this.props.teams[15].name}</p>
+                    <div className="east-first-round">
+                        <h3>East 1st Round</h3>
+                        <p>{this.props.teams[8].name} vs. {this.props.teams[9].name}</p>
+                        <p>{this.props.teams[10].name} vs. {this.props.teams[11].name}</p>
+                        <p>{this.props.teams[12].name} vs. {this.props.teams[13].name}</p>
+                        <p>{this.props.teams[14].name} vs. {this.props.teams[15].name}</p>
+                    </div>
+                    <div className="east-semis">
+                        <h3>East Semis</h3>
+                        <p>{this.props.teams[8].name} vs. {this.props.teams[9].name}</p>
+                        <p>{this.props.teams[10].name} vs. {this.props.teams[11].name}</p>
+                    </div>
+                    <div className="east-finals">
+                        <h3>East Finals</h3>
+                        <p>{this.props.teams[8].name} vs. {this.props.teams[9].name}</p>
+                    </div>
+                    <div className="east-winner">
+                        <h3>East Winner</h3>
+                        <p>{this.props.teams[8].name}</p>
+                    </div>                    
                 </div>
             </div>
         );
